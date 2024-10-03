443a825fdaa6f98e490ffcdf7445b6c8b0f01f21
omar <omaraziz.dev@gmail.com>
2024-10-03 13:58:11 -0700

    remove base

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 
 export default defineConfig({
-  // base is set for github pages
-  base: '/solidjs-webcontainers-xterm/',
   plugins: [
     solid(),
     {
