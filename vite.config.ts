81bbb4e11845930787f684e4f678c8fc6c7d6e58
omar <omaraziz.dev@gmail.com>
2024-10-03 13:50:28 -0700

    add cors

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,20 @@ import solid from 'vite-plugin-solid';
 export default defineConfig({
   // base is set for github pages
   base: '/solidjs-webcontainers-xterm/',
-  plugins: [solid()],
+  plugins: [
+    solid(),
+    {
+      name: 'add-cors',
+      configureServer(server) {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+          res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+          res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+          next();
+        });
+      },
+    },
+  ],
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
